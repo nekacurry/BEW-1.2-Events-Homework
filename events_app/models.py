b404591a850c78ec7238b95f259f359ba5020e95
nekacurry <anneka498@gmail.com>
2021-04-21 12:34:32 -0500

    Update models.py

--- events_app/models.py
+++ events_app/models.py
@@ -11,6 +11,10 @@ from sqlalchemy.orm import backref
 
 class Guest(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(25), nullable=False)
+    email = db.Column(db.String(50))
+    phone = db.Column(db.String(11))
+    events_attending = db.relationship('Event', secondary='guest_event', back_populates='guests')
 
 # TODO: Create a model called `Event` with the following fields:
 # - id: primary key
@@ -24,9 +28,16 @@ class Guest(db.Model):
 
 class Event(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50), nullable=False)
+    description = db.Column(db.String(100))
+    date_and_time = db.Column(db.Date)
+    guests = db.relationship('Guest', secondary='guest_event', back_populates='events_attending')
 
 # TODO: Create a table `guest_event_table` with the following columns:
 # - event_id: Integer column (foreign key)
 # - guest_id: Integer column (foreign key)
 
-guest_event_table = None
+guest_event_table = db.Table('guest_event',
+    db.Column('event_id', db.Integer, db.ForeignKey('event.id')),
+    db.Column('guest_id', db.Integer, db.ForeignKey('guest.id'))
+)
